d1fb7ac139d7d673a6184d86d480ed4fe0a87e44
Marc-Antoine Courteau <macourteau@gmail.com>
2023-08-07 08:34:45 -0400

    More issues if importing same package in app as in library.

--- demo/web/apps/my-web-app/src/app.ts
+++ demo/web/apps/my-web-app/src/app.ts
@@ -1,10 +1,14 @@
 import * as libA from '@demo/my-lib-a';
 import * as libB from '@demo/my-lib-b';
+import express from 'express';
 import http from 'node:http';
 
 console.log(`function from libA returns ${libA.libAFunction()}`);
 
-const server = http.createServer(libB.libBFunction());
+const app = libB.libBFunction();
+app.use(express.static('.'));
+
+const server = http.createServer(app);
 server.listen(3000);
 
 console.log(`listening on port 3000`);
